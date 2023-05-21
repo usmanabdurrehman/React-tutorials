494b45f45df58a696ab414a310086db94dc48e63
Usman Abdur Rehman <saidskit@outlook.com>
2023-05-21 20:41:25 +0500

    All Formik Hooks tutorial

---
+++ src/Components/AllFormikHooks/AllFormikHooks.tsx
@@ -0,0 +1,94 @@
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  NumberInput,
+  NumberInputField,
+} from "@chakra-ui/react";
+import {
+  ErrorMessage,
+  Field,
+  Form,
+  Formik,
+  useFormikContext,
+  useFormik,
+} from "formik";
+import { useEffect } from "react";
+
+import * as Yup from "yup";
+
+/*
+All Formik Hooks
+
+useFormik => use formik without React Context
+
+useFormikContext => get all formik props using Formik Context
+useField => fer field related formik props using formik context
+*/
+
+const ValidationSchema = Yup.object({
+  height: Yup.number().required("The height is required"),
+  weight: Yup.number().required("The weight is required"),
+});
+
+export const AllFormikHooks = () => {
+  return (
+    <Flex
+      height="100vh"
+      alignItems={"center"}
+      justifyContent="center"
+      bg="#bfffc9"
+    >
+      <Box boxShadow={"2xl"} width="350px" padding="10" rounded="xl" bg="white">
+        <Formik
+          onSubmit={async () => {}}
+          initialValues={{
+            height: undefined,
+            weight: undefined,
+          }}
+          validationSchema={ValidationSchema}
+          validateOnBlur={false}
+          validateOnChange={false}
+          validateOnMount={false}
+        >
+          {({ submitForm, errors }) => {
+            return (
+              <Form>
+                <h2>BMI Calculator</h2>
+                <Field name="height">
+                  {({ field }: any) => (
+                    <FormControl isInvalid={!!errors.height}>
+                      <FormLabel>Height in metres</FormLabel>
+                      <NumberInput>
+                        <NumberInputField {...field} />
+                      </NumberInput>
+                      <FormErrorMessage>{errors.height}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <Field name="weight">
+                  {({ field }: any) => (
+                    <FormControl isInvalid={!!errors.weight}>
+                      <FormLabel>Weight in kgs</FormLabel>
+                      <NumberInput>
+                        <NumberInputField {...field} />
+                      </NumberInput>
+                      <FormErrorMessage>{errors.weight}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <Button colorScheme={"whatsapp"} onClick={submitForm} mt={4}>
+                  Submit
+                </Button>
+              </Form>
+            );
+          }}
+        </Formik>
+      </Box>
+    </Flex>
+  );
+};
