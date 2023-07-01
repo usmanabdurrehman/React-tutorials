e4b942c02148b85ee9c69aa5faf6c16ad271ae8d
Usman Abdur Rehman <saidskit@outlook.com>
2023-07-01 23:21:53 +0500

    Added some more code

--- src/Components/TanstackQueryNetworkMode/TanstackQueryNetworkMode.tsx
+++ src/Components/TanstackQueryNetworkMode/TanstackQueryNetworkMode.tsx
@@ -11,7 +11,7 @@ export const TanstackQueryNetworkMode = () => {
       );
       return data;
     },
-    { networkMode: "offlineFirst" }
+    { networkMode: "offlineFirst" } // online | always | offlineFirst
   );
 
   return (
