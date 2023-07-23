4daafd28505ebe7e9aef3ac4ca4db2ec5d6aad8b
Usman Abdur Rehman <saidskit@outlook.com>
2023-07-23 13:31:38 +0500

    Tanstack Query Sync Fetching Tutorial

---
+++ src/Components/TanstackQuerySyncFetching/TanstackQuerySyncFetching.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+import { useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+
+export default function TanstackQuerySyncFetching() {
+  const [name, setName] = useState("");
+
+  const [error, setError] = useState("");
+  const queryClient = useQueryClient();
+
+  return (
+    <Flex
+      height="100vh"
+      alignItems={"center"}
+      justifyContent="center"
+      bg="#bfffc9"
+    >
+      <Box boxShadow={"2xl"} width="350px" padding="10" rounded="xl" bg="white">
+        <FormControl isInvalid={!!error}>
+          <FormLabel>Enter Name</FormLabel>
+          <Input onChange={(e) => setName(e.target.value)} value={name} />
+          <FormErrorMessage>{error}</FormErrorMessage>
+        </FormControl>
+
+        <Button
+          colorScheme={"whatsapp"}
+          onClick={async () => {
+            const users = await queryClient.fetchQuery(
+              ["FETCH_USERS"],
+              async (): Promise<{ name: string }[]> => {
+                const { data } = await axios.get(
+                  "https://jsonplaceholder.typicode.com/users"
+                );
+                return data;
+              },
+              { staleTime: 10000 }
+            );
+            const isUserDuplicate = users?.find((user) => user.name === name);
+            if (isUserDuplicate) setError("Duplicate Error");
+            else setError("");
+          }}
+          mt={4}
+        >
+          Validate
+        </Button>
+      </Box>
+    </Flex>
+  );
+}
