1cdd97417960bd85c8d42e419dc4d9394ffd0648
Usman Abdur Rehman <saidskit@outlook.com>
2023-09-13 15:08:40 +0500

    Added Code for New Optimistic Way

--- src/NewOptimistic/index.tsx
+++ src/NewOptimistic/index.tsx
@@ -3,10 +3,10 @@ import axios from "axios";
 
 export const NewOptimistic = () => {
   const { data } = useQuery({
-    queryKey: ["GET_TODOS"],
+    queryKey: ["GET_USERS"],
     queryFn: async () => {
       const { data } = await axios.get(
-        "https://jsonplaceholder.typicode.com/todos"
+        "https://jsonplaceholder.typicode.com/users"
       );
       return data;
     },
@@ -14,20 +14,28 @@ export const NewOptimistic = () => {
   const queryClient = useQueryClient();
 
   const { mutate, isPending, variables } = useMutation({
-    mutationFn: (newTodo: string) =>
-      axios.post("https://jsonplaceholder.typicode.com/todos", {
-        text: newTodo,
+    mutationFn: (newUser: string) =>
+      axios.post("https://jsonplaceholder.typicode.com/users", {
+        name: newUser,
       }),
-    onSettled: () => queryClient.invalidateQueries({ queryKey: ["todos"] }),
+    onSettled: (data) =>
+      queryClient.setQueryData(["GET_USERS"], (users: any[] | undefined) => [
+        ...(users || []),
+        data?.data,
+      ]),
   });
+
+  console.log({ data });
 
   return (
     <div style={{ margin: 20 }}>
       <ul>
-        {data?.slice(0, 5)?.map((todo: any) => (
-          <li key={todo.id}>{todo.title}</li>
+        {data?.map((todo: any) => (
+          <li key={todo.id}>{todo.name}</li>
         ))}
-        {isPending && <li>{variables}</li>}
+        {isPending && (
+          <li style={{ opacity: isPending ? 0.5 : 1 }}>{variables}</li>
+        )}
       </ul>
       <button onClick={() => mutate("Grocery")} style={{ marginLeft: 15 }}>
         Add Grocery todo
--- src/dummy.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const QUERY_KEY = ["GET_USERS"];
-export const QUERY_FN = async () => {
-  const { data } = await axios.get("some/url");
-  return data;
-};
