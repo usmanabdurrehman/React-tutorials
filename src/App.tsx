20c59701d7e32292afb4b55a4644a7d79bdffd51
Usman Abdur Rehman <saidskit@outlook.com>
2023-10-06 19:33:33 +0500

    some konva code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { BasicTable } from "./Components/BasicTable";
+import { DiagramEditor } from "./Components/X";
 
 export default function App() {
   return (
     <ChakraProvider>
-      <BasicTable />
+      {/* <BasicTable /> */}
+      <DiagramEditor />
     </ChakraProvider>
   );
 }
