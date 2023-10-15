311266aa5310f34aa7bad8bf42be55b6414d722f
Usman Abdur Rehman <saidskit@outlook.com>
2023-10-15 23:50:52 +0500

    Add SetHasCode

--- src/Components/List.tsx
+++ src/Components/List.tsx
@@ -3,14 +3,14 @@ import moment from "moment";
 import React from "react";
 import { users } from "./data";
 
-const verifiedUserIds = [users[0].id, users[3].id];
+const verifiedUserIds = new Set([users[0].id, users[3].id]);
 
 export default function List() {
   return (
     <Flex direction={"column"} gap={4} m={4} width={500}>
       {users.map((user) => (
         <Box key={user.id} boxShadow="lg" p={4}>
-          {verifiedUserIds.find((userId) => userId === user.id) && (
+          {verifiedUserIds.has(user.id) && (
             <Badge colorScheme="green">Verified</Badge>
           )}
           <Text fontSize={"xl"} mb={2}>
