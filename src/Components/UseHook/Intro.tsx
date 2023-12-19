74873433fb3f525c40642e64d2f3ff46b7bfd0c1
Usman Abdur Rehman <saidskit@outlook.com>
2023-12-20 00:57:34 +0500

    Added Intro File

---
+++ src/Components/UseHook/Intro.tsx
@@ -0,0 +1,16 @@
+// @ts-ignore
+import { use } from "react";
+
+const getCards = fetch("http://localhost:7000/cards").then((res) => res.json());
+const getCard = (id: number) =>
+  fetch(`http://localhost:7000/cards/${id}`).then((res) => res.json());
+
+const cardIds = [1, 2, 3];
+
+export const Card = ({ shouldFetch }: { shouldFetch?: boolean }) => {
+  if (shouldFetch) use(getCards);
+
+  cardIds.forEach((cardId) => {
+    use(getCard(cardId));
+  });
+};
